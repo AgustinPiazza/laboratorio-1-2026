7901138a9b84cf366e581983b1c37b6cdc03b969
AgustinPiazzaIT4 <agustin.piazza@it4next.com>
2026-09-01 20:45:10 -0300

    ejs

--- src/ar/edu/unlu/poo/labo1/artefactos/Artefacto.java
+++ src/ar/edu/unlu/poo/labo1/artefactos/Artefacto.java
@@ -1,4 +1,37 @@
 package ar.edu.unlu.poo.labo1.artefactos;
 
+import java.util.Objects;
+
 public class Artefacto {
+    private String nombre;
+    private Integer poder;
+    private String tipo;
+
+    public Artefacto(String unNombre, int unPoder, String unTipo) {
+        nombre = unNombre;
+        poder = unPoder;
+        tipo = unTipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public String getTipo() {
+        return tipo;
+    }
+    public Integer getPoder() {
+        return poder;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this==obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Artefacto artefacto = (Artefacto) obj;
+        return Objects.equals(nombre, artefacto.nombre);
+    }
+
+    public int hashCode() {
+        return Objects.hash(nombre);
+    }
 }
--- src/ar/edu/unlu/poo/labo1/artefactos/CatalogoArtefactos.java
+++ src/ar/edu/unlu/poo/labo1/artefactos/CatalogoArtefactos.java
@@ -1,4 +1,35 @@
 package ar.edu.unlu.poo.labo1.artefactos;
 
+import java.util.*;
+
 public class CatalogoArtefactos {
+
+    public Set<Artefacto> catalogo;
+
+    public void agregarArtefacto(Artefacto unArtefacto) {
+        Set<Artefacto> catalogo = new HashSet<>();
+
+        catalogo.add(unArtefacto);
+    }
+
+    public Set<Artefacto> obtenerArtefactosUnicos(){
+        return catalogo;
+    }
+
+    public List<Artefacto> listaArtefactos;
+
+    public List<Artefacto> buscarArtefactosPorTipo(String unTipo) {
+        List<Artefacto> listaArtefactos = new ArrayList<>();
+
+    }
+
+    public Map<String, Integer> artefactosPorTipo;
+    public String getTipo;
+
+    public Map<String, Integer> contarArtefactosPorTipo() {
+        Map<String, Integer> artefactosPorTipo = new HashMap<>();
+
+        int conteo = artefactosPorTipo.get(String);
+        artefactosPorTipo.put(String, conteo + 1);
+    }
 }
